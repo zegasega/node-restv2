d1a1528dd072aa1d01b3b39844b82c5221df0499
GitHub kullanıcı adınız <GitHub email adresiniz>
2025-05-05 21:31:35 +0300

    UserModelField&Utils

--- models/user.js
+++ models/user.js
@@ -4,11 +4,36 @@ const sequelize = require('../config/database');
 class User extends Model {}
 
 User.init({
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [2, 50],
+      notEmpty: { msg: 'İsim boş bırakılamaz' }
+    }
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+      notEmpty: { msg: 'Email boş bırakılamaz' }
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [6, 100],
+      notEmpty: { msg: 'Parola boş bırakılamaz' }
+    }
+  }
 }, {
   sequelize,
   modelName: 'User',
+  tableName: 'users',
+  timestamps: true,
 });
 
 module.exports = User;
--- validations/userValidation.js
+++ validations/userValidation.js
@@ -3,10 +3,12 @@ const Joi = require('joi');
 const userSchema = Joi.object({
   name: Joi.string().min(3).required(),
   email: Joi.string().email().required(),
+  password: Joi.string().min(3).required()
 });
 
 module.exports = { userSchema };
 
 // her crud islemi icin yeni bir validation yaziyor mesela update user yaparken farki validation yapabilirmiyiz
 // bir model icin (user) tek bir validation yazilip kullanilabilirmi
-//
+
+// model kismindada fieldlara kural koyduk, joi validator ile de koyuyoruz ikiside gereklimi
